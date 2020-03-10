947dd067afa1e7b70b487cefbe62034b12451732
15283823275 <625783219@qq.com>
2020-03-10 10:57:14 +0800

    delete webapp

--- webapp/src/i18n/lang/en.js
+++
@@ -1,9 +0,0 @@
-export default {
-    header_home:'Home',
-    header_community:'社区',
-    header_user:'用户',
-    header_efficiency:'高效',
-    header_doc:'文档',
-    header_down:'下载',
-    header_security:'安全',
-}
