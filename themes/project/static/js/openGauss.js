2a8bdb199de5d41690306bdc4f7a54661bfb775f
wangpeiheng <805075854@qq.com>
2020-06-09 21:23:42 +0800

    modify page style

--- themes/project/static/js/openGauss.js
+++ themes/project/static/js/openGauss.js
@@ -1,6 +1,6 @@
   var myBannerSwiper = new Swiper ('.banner_swiper', {
     direction: 'horizontal', // 垂直切换选项
-    loop: true, // 循环模式选项
+    /* loop: true, */ // 循环模式选项
     autoplay:true,
     
     // 如果需要分页器
