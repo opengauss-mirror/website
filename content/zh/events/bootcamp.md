f8e67628e8cd378d845a32044e057a8c868d162c
LiXinXing <969966273@qq.com>
2020-09-22 16:23:57 +0800

    add nav style in mobile

--- content/zh/events/bootcamp.md
+++ content/zh/events/bootcamp.md
@@ -12,6 +12,8 @@ summary = "面向开发者的一次盛会，欢迎大家参加。"
 
 ### 活动介绍
 
+本次开发者训练营活动以演讲和交流的方式进行，我们将邀请openGauss社区布道师李思昊和openGauss首席架构师田文罡向大家分享关于硬件演变对开源数据库发展带来的影响以及openGauss的技术演进等内容。本次活动将全程开启直播，并将开启多轮抽奖去，敬请关注！
+
 ### 时间
 
 2020-9-25 13:30 -15:30
@@ -27,24 +29,24 @@ http://live.huawei.com/huaweiconnect/meeting/cn/5869.html
 
 ### 活动议程
 
-<img src="/img/events/bootcamp/live_event.png" width="30%" style="margin-bottom: 0.2rem;" />
+<img src="/img/events/bootcamp/live_event.png" width="100%" style="margin-bottom: 0.2rem; max-width: 1024px" />
 
-议题1：硬件演变对开源数据库发展有怎样的影响
+<em>议题1：</em>硬件演变对开源数据库发展有怎样的影响
 
-议题简介：自从数据库诞生开始，硬件的发展一直驱动着数据库技术的演进。当前异构计算、非易失性内存和RDMA高速网络等新硬件技术即将触手可得，这个议题将解密新硬件技术对开源数据库带来从架构到关键技术等各层面的重大影响。
+<em>议题简介：</em>自从数据库诞生开始，硬件的发展一直驱动着数据库技术的演进。当前异构计算、非易失性内存和RDMA高速网络等新硬件技术即将触手可得，这个议题将解密新硬件技术对开源数据库带来从架构到关键技术等各层面的重大影响。
  
-活动嘉宾：李思昊       openGauss社区布道师、中国家算计协会数据可专委会委员、华为2012实验室高斯部技术专家
+<em>活动嘉宾：</em>李思昊       openGauss社区布道师、中国家算计协会数据可专委会委员、华为2012实验室高斯部技术专家
 
 
-议题2：openGauss 应用实践的那些年那些事
+<em>议题2：</em>openGauss 应用实践的那些年那些事
 
-议题简介：通过openGauss在应用中的实践，介绍openGauss的技术演进。
+<em>议题简介：</em>通过openGauss在应用中的实践，介绍openGauss的技术演进。
 
-活动嘉宾： 田文罡        openGauss社区技术委员会主席、 华为数据库C-TMG主任、 16年数据库开发经验。
+<em>活动嘉宾：</em> 田文罡        openGauss社区技术委员会主席、 华为数据库C-TMG主任、 16年数据库开发经验。
 
 
 
-关注openGauss微信公众号，了解我们更多活动信息。
+<em>关注openGauss微信公众号，了解我们更多活动信息。</em>
 
 <img src="/img/events/bootcamp/live_weChat_code.jpg" width="30%" style="margin-bottom: 0.2rem;" />
 
--- themes/project/static/js/common.js
+++ themes/project/static/js/common.js
@@ -26,7 +26,6 @@ $('#nav .list').each(function () {
     })
 })
 
-
 // 监听滚动条 当大于 50的时候 关闭二级菜单
 $(window).scroll(function () {
     var scrollTop = $(window).scrollTop();
@@ -34,3 +33,43 @@ $(window).scroll(function () {
         hideNav();
     }
 });
+
+var judgeNavs = function (str, navs) {
+    var home = ['https://opengauss.org/zh/', 'https://opengauss.org/en/', 'http://localhost:1313/en/', 'http://localhost:1313/zh/']
+    for (let h of home) {
+        if (str === h) {
+            return 'home'
+        }
+    }
+    for (let nav of navs) {
+        if (str.includes(nav)) {
+            return nav
+        }
+    }
+    return false
+}
+
+var mobileNavEvent = function () {
+    var href = location.href
+    var navs = ['lang', 'home', 'download', 'docs', 'contribution', 'onlineCommunication', 'onlineMeeting', 'security', 'news', 'events', 'blog', 'video', 'wechat']
+    var current = judgeNavs(href, navs)
+    if (current) {
+        for (let i = 0; i < navs.length; i++) {
+            var nav = navs[i]
+            if (nav === current) {
+                $("a.list").eq(i).addClass('active');
+                break
+            }
+        }
+    }
+    if (href.includes('community') || href.includes('contribution')) {
+        $('p.second_menu').addClass('active')
+    } else {
+        $('p.second_menu').removeClass('active')
+    }
+}
+
+// 移动端导航栏添加样式
+if ($(window).width() < 1280) {
+    mobileNavEvent()
+}
