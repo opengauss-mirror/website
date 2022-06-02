5c5d15f4ebff5c73142da5d8c23b307fbb7b5b82
ailoooong <ilong8@qq.com>
2022-06-02 15:00:15 +0800

    fix：user story content

--- content/zh/userStory/_index.md
+++ content/zh/userStory/_index.md
@@ -17,6 +17,7 @@ name = "DBV"
 identification = "isv"
 name = "ISV"
 
+
 [[list]]
 identification = "finance"
 name = "金融/银行"
@@ -25,9 +26,11 @@ name = "金融/银行"
 identification = "developer"
 name = "互联网开发/软件开发"
 
+
 [[list]]
 identification = "industrial"
 name = "工业/制造"
+
 
 [[list]]
 identification = "other"
@@ -36,38 +39,38 @@ name = "其他"
 
 [[cards]]
 iden = "developer"
-name = "中软国际科技与服务有限公司"
-desc = "openGauss生态合作伙伴，提供配套开发与管理、运维工具。"
-address = "https://www.chinasofti.com/"
-weight = 1
-
-[[cards]]
-iden = "developer"
-name = "江苏润和软件股份有限公司"
-desc = "应用在部分电力行业的系统开发中，为高并发的数据访问提供支撑。部署openGauss服务器节点数为11~20个。"
-address = "http://www.hoperun.com/"
-weight = 2
-
-[[cards]]
-iden = "developer"
-name = "SphereEx"
-desc = "构建了Apache ShardingSphere + openGauss 的分布式解决方案，突破了单机性能瓶颈，使用 16 台服务器在超过 1 小时的测试中，得到了平均超过 1000 万 tpmC 的结果。部署openGauss服务器节点数为11~20个。"
-address = "https://www.sphere-ex.com/zh/"
-weight = 4
-
-[[cards]]
-iden = "developer"
 name = "北京江南天安科技有限公司"
 desc = "将openGauss应用在公司相关产品中，作为数据存储数据库使用，部署openGauss服务器节点数为11~20个。"
 address = "https://www.tass.com.cn/"
-weight = 5
+weight = 1
 
 [[cards]]
 iden = "developer"
 name = "DataPipeline"
 desc = "与openGauss形成联合解决方案，服务金融 电信 能源等行业客户。"
 address = "https://www.datapipeline.com/"
-weight = 6
+weight = 2
+
+[[cards]]
+iden = "developer"
+name = "江苏润和软件股份有限公司"
+desc = "应用在部分电力行业的系统开发中，为高并发的数据访问提供支撑。部署openGauss服务器节点数为11~20个。"
+address = "http://www.hoperun.com/"
+weight = 3
+
+[[cards]]
+iden = "developer"
+name = "SphereEx"
+desc = "构建了Apache ShardingSphere + openGauss 的分布式解决方案，突破了单机性能瓶颈，使用 16 台服务器在超过 1 小时的测试中，得到了平均超过 1000 万 tpmC 的结果。部署openGauss服务器节点数为11~20个。"
+address = "https://www.sphere-ex.com/zh/"
+weight = 4 
+
+[[cards]]
+iden = "developer"
+name = "中软国际科技与服务有限公司"
+desc = "openGauss生态合作伙伴，提供配套开发与管理、运维工具。"
+address = "https://www.chinasofti.com/"
+weight = 5
 
 
 [[cards]]
@@ -78,48 +81,57 @@ address = "https://www.bessystem.com/"
 weight = 1
 
 
-[[cards]]
-iden = "dbv"
-name = "天津神舟通用数据技术有限公司"
-desc = "神通公司基于openGauss发行商业版神通数据库V7.0.21版本，广泛应用在了信息创新领域的系统中！部署openGauss服务器节点数为50个以上。"
-address = "http://www.shentongdata.com/"
-weight = 1
+
 
 [[cards]]
 iden = "dbv"
 name = "北京海量数据股份有限公司"
 desc = "使用openGauss做海量数据库Vastbase商业发行版，部署openGauss服务器节点数为1~10个。"
 address = "http://www.vastdata.com.cn/"
-weight = 2
+weight = 1
 
 [[cards]]
 iden = "dbv"
 name = "北京太阳塔信息科技有限责任公司"
 desc = "使用openGauss做了LNXDB数据库的商业发行版，推给新客户使用，同时正在将推出的LNXDB-RDS for  openGauss 版本的数据库一体机，推向政府类客户。部署openGauss服务器节点数为1~10个。"
 address = "http://www.taryartar.com/"
-weight = 3
+weight = 2
 
 [[cards]]
 iden = "dbv"
 name = "成都虚谷伟业科技有限公司"
 desc = "在openGuass 多线程绑核技术基础上进行数据库开发，部署openGauss服务器节点数为1~10个。"
 address = "http://www.xugucn.com/"
-weight = 4
-
+weight = 3
 
 [[cards]]
-iden = "finance"
-name = "民生银行"
-desc = "承载除核心外的其他交易类非交易类业务，部署openGauss服务器节点数为50个以上。"
-address = "http://www.cmbc.com.cn/"
-weight = 1
+iden = "dbv"
+name = "天津神舟通用数据技术有限公司"
+desc = "神通公司基于openGauss发行商业版神通数据库V7.0.21版本，广泛应用在了信息创新领域的系统中！部署openGauss服务器节点数为50个以上。"
+address = "http://www.shentongdata.com/"
+weight = 4
+
+[[cards]]
+iden = "dbv"
+name = "云和恩墨（北京）信息技术有限公司"
+desc = "云和恩墨基于openGauss内核进行增强提升，推出的商业发行版MogDB，MogDB是一款安稳易用的企业级关系型数据库，广泛应用于银行、保险、政府、能源和电信领域生产场景中。部署openGauss服务器节点数为50个以上。"
+address = "https://enmotech.com/"
+weight = 5
+
 
 [[cards]]
 iden = "finance"
 name = "北京沐融信息科技股份有限公司"
 desc = "银行核心系统支付系统收单系统，部署openGauss服务器节点数为11~20个。"
 address = "http://www.murongtech.com/"
-weight = 2 
+weight = 1
+
+[[cards]]
+iden = "finance"
+name = "民生银行"
+desc = "承载除核心外的其他交易类非交易类业务，部署openGauss服务器节点数为50个以上。"
+address = "http://www.cmbc.com.cn/"
+weight = 2
 
 [[cards]]
 iden = "finance"
@@ -150,7 +162,12 @@ desc = "做为一家提供可计算性存储的公司，跟openGauss结合使用
 address = "http://www.scaleflux.cn/"
 weight = 2
 
-
+[[cards]]
+iden = "industrial"
+name = "沈阳航兴科技有限公司"
+desc = "全新部署在x86服务器上，部署openGauss服务器节点数为1~10个。"
+address = "www.syhangxing.com"
+weight = 3
 
 +++
 
@@ -172,15 +189,20 @@ name = "医疗/生物技术/其他科学"
 identification = "energy"
 name = "能源/电力"
 
-
-
 [[list]]
-identification = "carriers "
+identification = "carriers"
 name = "运营商（移动、电信、联通、广电）"
 
 [[list]]
-identification = "oilGas "
+identification = "oilGas"
 name = "油气"
 
+[[list]]
+identification = "transportation"
+name = "交通"
+
+[[list]]
+identification = "safeCity"
+name = "安平"
 
  -->
--- themes/project/static/js/user-story.js
+++ themes/project/static/js/user-story.js
@@ -1,59 +1,58 @@
 $(document).ready(function () {
-$('.left-tools').find('div').on('click', function (e) {
-    e.preventDefault();
-    $(this).addClass('active').siblings().removeClass('active');
-    let id = $(this).find('a').attr('href');
-    let toTop = ['#dbv', '#isv', '#developer', '#finance', '#other', '#government', '#education', '#science', '#energy','#industrial','#carriers','#oilGas'];
-    toTop.forEach((item, index) => {
-     if (item === id){
-         let h = $(id).offset().top - 60;
-         window.scrollTo(0, h)
-     }
-    });
-});
-
-if ($(window).width() < 1000) {
-    $('.stretch-img').each(function () {
-        new RTP.PinchZoom($(this), {});
-    });
-    $('.isH5 .tool-detail').find('.tool-item').find('.item-title').on('click', function () {
-        $(this).siblings('.item-detail').toggleClass('hide');
-
-        let url = $(this).find('img').attr('src');
-        if (url.includes('right')) {
-            $(this).find('img').attr('src', '/img/swiper/down.svg');
-        } else {
-            $(this).find('img').attr('src', '/img/swiper/right.svg');
-        }
+    var menuListId = []
+    $('.left-tools div').each(function () {
+        menuListId.push($(this).find('a').attr('href'))
     })
-} else {
-    $(window).scroll(function () {
-        let top = $(window).scrollTop();
-        // console.log('top', top)
-        if (top <= 755) {
-            $('.left-tools div:nth-child(1)').addClass('active').siblings().removeClass('active');
-        } else if ((top >= 755) && (top < 955)) {
-            $('.left-tools div:nth-child(2)').addClass('active').siblings().removeClass('active');
-        } else if ((top > 955) && (top <= 1330)) {
-            $('.left-tools div:nth-child(3)').addClass('active').siblings().removeClass('active');
-        } else if ((top >= 1330) && (top < 1885)) {
-            $('.left-tools div:nth-child(4)').addClass('active').siblings().removeClass('active');
-        } else if ((top >= 1806) && (top < 2188)) {
-            $('.left-tools div:nth-child(5)').addClass('active').siblings().removeClass('active');
-        } else if ((top >= 2188) && (top < 2380)) {
-            $('.left-tools div:nth-child(6)').addClass('active').siblings().removeClass('active');
-        } else if ((top >= 2380) && (top < 2680)) {
-            $('.left-tools div:nth-child(7)').addClass('active').siblings().removeClass('active');
-        } else if ((top >= 2680) && (top < 2880)) {
-            $('.left-tools div:nth-child(8)').addClass('active').siblings().removeClass('active');
-        }else if (top >= 2880) {
-            $('.left-tools div:nth-child(9)').addClass('active').siblings().removeClass('active');
-        }
-        // if (top >= 880) {
-        //     $('.left-tools').removeClass('hide')
-        // } else {
-        //     $('.left-tools').addClass('hide')
-        // }
-    });
-};
-});
+    $('.left-tools div').on('click', function (e) {
+        e.preventDefault()
+        $(this).addClass('active').siblings().removeClass('active')
+        let id = $(this).find('a').attr('href')
+        menuListId.forEach((item, index) => {
+            if (item === id) {
+                let h = $(id).offset().top - 60
+                window.scrollTo(0, h)
+            }
+        })
+    })
+
+    if ($(window).width() < 1000) {
+        $('.stretch-img').each(function () {
+            new RTP.PinchZoom($(this), {})
+        })
+        $('.isH5 .tool-detail')
+            .find('.tool-item')
+            .find('.item-title')
+            .on('click', function () {
+                $(this).siblings('.item-detail').toggleClass('hide')
+
+                let url = $(this).find('img').attr('src')
+                if (url.includes('right')) {
+                    $(this).find('img').attr('src', '/img/swiper/down.svg')
+                } else {
+                    $(this).find('img').attr('src', '/img/swiper/right.svg')
+                }
+            })
+    } else {
+        $(window).scroll(function () {
+            var targetUrl = []
+            var targetUrlArr = []
+            $('.left-tools div').each(function () {
+                targetUrlArr.push($($(this).find('a').attr('href')))
+            })
+            try {
+                targetUrl = targetUrlArr.filter(function (item) {
+                    return $(window).scrollTop() + 80 > item.offset().top
+                })
+
+                if (targetUrl.length) {
+                    $('.left-tools div')
+                        .removeClass('active')
+                        .eq(targetUrl.length - 1)
+                        .addClass('active')
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        })
+    }
+})
