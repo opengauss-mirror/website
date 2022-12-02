427c53c84a12e0b3abff88ccee3c036de01c0e25
ailoooong <ilong8@qq.com>
2022-12-02 16:22:08 +0800

    fix：新增openGauss商标使用规范

--- app/.vitepress/src/i18n/brand/index.ts
+++ app/.vitepress/src/i18n/brand/index.ts
@@ -3,8 +3,12 @@ export default {
     BRAND: '品牌',
     WORDS:
       '注意：openGauss网站上使用和显示的所有商标、标志皆属openGauss社区所有，但注明属于其他方拥有的商标、标志除外。未经openGauss社区或其他方书面许可，openGauss网站所载的任何内容不应被视作以暗示、不反对或其他形式授予使用前述任何商标、标志的许可或权利。未经事先书面许可，任何人不得以任何方式使用openGauss社区的名称及openGauss社区的商标、标记，除非你使用前述名称、商标和标记是出于个人、教育和非商业目的。任何人不得在未获得第三方同意的情况下使用第三方拥有的商标和标志。',
-
-    FAQ_LINK: 'mailto:trademarks@openeuler.sh',
+    WORDS1: [
+      '当您在实际使用openGauss品牌商标时，请注意遵循',
+      '《openGauss商标使用规范》 。',
+      '/file/brand/openGauss商标使用规范.pdf',
+    ],
+    FAQ_LINK: 'mailto:contact@opengauss.org',
     MOBILETITLE: '商标',
     PPT_TEXT: 'PPT模板',
   },
@@ -12,8 +16,12 @@ export default {
     BRAND: 'Brand',
     WORDS:
       'Notice: All logos and trademarks used on this website are the properties of openGauss community or other third parties as stated if applicable. No content provided on the openGauss community website shall be deemed as granting approval or the right to use any trademark or logo aforesaid by implication, lack of objection, or other means without the prior written consent of openGauss community or any third party which may own the mark. No individual shall use the name, trademark, or logo of openGauss community by any means without the prior written consent of openGauss community.',
-
-    FAQ_LINK: 'mailto:trademarks@openeuler.sh',
+    WORDS1: [
+      'When using the openGauss trademarks, comply with the',
+      ' openGauss Trademark Usage Specifications.',
+      '/file/brand/openGauss商标使用规范.pdf',
+    ],
+    FAQ_LINK: 'mailto:contact@opengauss.org',
     MOBILETITLE: 'Trademark',
     PPT_TEXT: 'PPT Templates',
   },
