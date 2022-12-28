2bd9e76c74d20a704d6dc3a11d740b31adb76d8c
ailoooong <ilong8@qq.com>
2022-12-28 19:13:33 +0800

    fix：summit 议程

--- app/.vitepress/src/views/summit/summit2022/TheSummit2022.vue
+++ app/.vitepress/src/views/summit/summit2022/TheSummit2022.vue
@@ -56,6 +56,18 @@ onMounted(() => {
     setDownData();
   }, 300);
 });
+
+// video 事件
+const videoDialog = ref(false);
+const videoLink = ref('');
+const handleCloseVideo = () => {
+  videoDialog.value = false;
+  videoLink.value = '';
+};
+const videoClickBtn = (path: string) => {
+  videoLink.value = path;
+  videoDialog.value = true;
+};
 </script>
 <template>
   <div class="banner">
@@ -148,7 +160,38 @@ onMounted(() => {
         </OContainer>
       </div>
     </div>
-
+    <!-- 线上展厅 
+    <h3 class="titleBar">线上展厅</h3>
+    <div class="exhibition-online">
+      <a
+        v-for="item in SUMMITDATA.videolist"
+        :key="item.name"
+        href=""
+        :name="item.name"
+        @click="videoClickBtn(item.link)"
+      ></a>
+      <div v-if="videoDialog" class="video-box">
+        <ODialog
+          v-model="videoDialog"
+          :before-close="handleCloseVideo"
+          :show-close="false"
+          lock-scroll
+          close-on-press-escape
+          close-on-click-modal
+          width="800px"
+        >
+          <div class="video-center">
+            <video
+              class="exhibition-video"
+              :src="videoLink"
+              width="100%"
+              controls
+              autoplay
+            ></video>
+          </div>
+        </ODialog>
+      </div>
+    </div>-->
     <div class="summit-partners">
       <h3 class="titleBar">{{ SUMMITDATA.partnersList.title[0] }}</h3>
       <h4 class="meetingtitle">
@@ -199,23 +242,7 @@ onMounted(() => {
         </a>
       </div>
     </div>
-    <!-- <div class="content">
-      <div
-        v-for="item in SUMMITDATA.contentList"
-        :key="item.link"
-        class="content-item"
-      >
-        <a :href="item.link" target="_blank">
-          <div class="text">
-            <p>CALL FOR</p>
-            <p>{{ item.nameEn }}</p>
-            <p>{{ item.name }}</p>
-          </div>
 
-          <img :src="item.img" :alt="item.name" />
-        </a>
-      </div>
-    </div> -->
     <div class="previous">
       <div class="previous-title">
         <h3>{{ SUMMITDATA.previous.title }}</h3>
@@ -767,6 +794,488 @@ onMounted(() => {
         font-size: var(--o-font-size-h8);
         line-height: var(--o-line-height-h8);
       }
+    }
+  }
+}
+
+.exhibition-online {
+  width: 1416px;
+  height: 844px;
+  position: relative;
+  margin: 0 auto;
+  background: url(@/assets/category/summit/summit2022/exhibition.png) no-repeat;
+  background-size: 100% 100%;
+  @media screen and (max-width: 1460px) {
+    width: 1100px;
+    height: 536px;
+  }
+  @media screen and (max-width: 1100px) {
+    width: 100%;
+    height: 58vw;
+  }
+  a {
+    position: absolute;
+    background: rgba($color: #ff0000, $alpha: 0.3);
+    border-radius: 30px;
+    &:nth-of-type(1) {
+      width: 230px;
+      height: 58px;
+      left: 384px;
+      top: 43px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        top: 8px;
+        left: 284px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 1vw;
+        left: 23vw;
+      }
+    }
+    &:nth-of-type(2) {
+      width: 178px;
+      height: 58px;
+      left: 681px;
+      top: 44px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        top: 8px;
+        left: 284px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 1vw;
+        left: 23vw;
+      }
+    }
+    &:nth-of-type(3) {
+      width: 195px;
+      height: 52px;
+      right: 152px;
+      top: 169px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        top: 8px;
+        left: 458px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 1vw;
+        left: 38vw;
+      }
+    }
+    &:nth-of-type(4) {
+      width: 195px;
+      height: 54px;
+      right: 127px;
+      top: 293px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        top: 8px;
+        right: 310px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 1vw;
+        right: 25vw;
+      }
+    }
+    &:nth-of-type(5) {
+      width: 195px;
+      height: 54px;
+      right: 97px;
+      top: 418px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        top: 8px;
+        right: 138px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 1vw;
+        right: 11vw;
+      }
+    }
+    &:nth-of-type(6) {
+      width: 202px;
+      height: 58px;
+      left: 140px;
+      top: 244px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        top: 154px;
+        left: 108px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 16.5vw;
+        left: 9vw;
+      }
+    }
+    &:nth-of-type(7) {
+      width: 202px;
+      height: 58px;
+      left: 552px;
+      top: 244px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 428px;
+        top: 154px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 16.5vw;
+        left: 35vw;
+      }
+    }
+    &:nth-of-type(8) {
+      width: 202px;
+      height: 58px;
+      left: 342px;
+      top: 317px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 266px;
+        top: 198px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 21.5vw;
+        left: 22vw;
+      }
+    }
+    &:nth-of-type(9) {
+      width: 202px;
+      height: 58px;
+      left: 112px;
+      top: 398px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 86px;
+        top: 251px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 27vw;
+        left: 7vw;
+      }
+    }
+    &:nth-of-type(10) {
+      width: 202px;
+      height: 58px;
+      left: 556px;
+      top: 398px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 428px;
+        top: 251px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 27vw;
+        left: 35vw;
+      }
+    }
+    &:nth-of-type(11) {
+      width: 202px;
+      height: 58px;
+      right: 380px;
+      top: 244px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        right: 293px;
+        top: 154px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 16.5vw;
+        right: 24vw;
+      }
+    }
+    &:nth-of-type(12) {
+      width: 202px;
+      height: 58px;
+      right: 366px;
+      top: 368px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        right: 285px;
+        top: 232px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        top: 25vw;
+        right: 23vw;
+      }
+    }
+    &:nth-of-type(13) {
+      width: 172px;
+      height: 62px;
+      right: 114px;
+      top: 255px;
+      @media screen and (max-width: 1460px) {
+        width: 138px;
+        height: 38px;
+        right: 84px;
+        top: 160px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 12%;
+        height: 4vw;
+        top: 17.5vw;
+        right: 7vw;
+      }
+    }
+    &:nth-of-type(14) {
+      width: 202px;
+      height: 58px;
+      left: 246px;
+      bottom: 218px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 190px;
+        bottom: 136px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 15vw;
+        left: 15vw;
+      }
+    }
+    &:nth-of-type(15) {
+      width: 202px;
+      height: 58px;
+      left: 476px;
+      bottom: 218px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 371px;
+        bottom: 136px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 15vw;
+        left: 30vw;
+      }
+    }
+    &:nth-of-type(16) {
+      width: 202px;
+      height: 58px;
+      left: 707px;
+      bottom: 218px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 548px;
+        bottom: 136px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 15vw;
+        left: 45vw;
+      }
+    }
+    &:nth-of-type(17) {
+      width: 202px;
+      height: 58px;
+      right: 276px;
+      bottom: 218px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        right: 214px;
+        bottom: 136px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 15vw;
+        right: 18vw;
+      }
+    }
+    &:nth-of-type(18) {
+      width: 202px;
+      height: 58px;
+      right: 46px;
+      bottom: 218px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        right: 35px;
+        bottom: 136px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 15vw;
+        right: 3vw;
+      }
+    }
+    &:nth-of-type(19) {
+      width: 202px;
+      height: 58px;
+      left: 235px;
+      bottom: 101px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 183px;
+        bottom: 62px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 6.5vw;
+        left: 15vw;
+      }
+    }
+    &:nth-of-type(20) {
+      width: 202px;
+      height: 58px;
+      left: 472px;
+      bottom: 101px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 368px;
+        bottom: 62px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 6.5vw;
+        left: 30vw;
+      }
+    }
+    &:nth-of-type(21) {
+      width: 202px;
+      height: 58px;
+      left: 712px;
+      bottom: 101px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        left: 554px;
+        bottom: 62px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 6.5vw;
+        left: 45vw;
+      }
+    }
+    &:nth-of-type(22) {
+      width: 202px;
+      height: 58px;
+      right: 264px;
+      bottom: 101px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        right: 204px;
+        bottom: 62px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 6.5vw;
+        right: 17vw;
+      }
+    }
+    &:nth-of-type(23) {
+      width: 202px;
+      height: 58px;
+      right: 26px;
+      bottom: 101px;
+      border-radius: 29px;
+      @media screen and (max-width: 1460px) {
+        width: 158px;
+        height: 38px;
+        right: 18px;
+        bottom: 62px;
+        border-radius: 19px;
+      }
+      @media screen and (max-width: 1100px) {
+        width: 15%;
+        height: 4vw;
+        bottom: 6.5vw;
+        right: 1vw;
+      }
+    }
+  }
+  .video-box {
+    :deep(.el-dialog__header) {
+      display: none;
+    }
+    :deep(.el-dialog__body) {
+      padding: 0;
+    }
+    .exhibition-video {
+      display: block;
+      margin: 0 auto;
+      width: 100%;
     }
   }
 }
