257f33170c1c3f411609d422106622fb379c4e4e
Bang <15622356989@163.com>
2023-01-09 22:29:27 +0800

    fix: 首页链接调整为3.1.1

--- app/.vitepress/src/i18n/common/common-zh.ts
+++ app/.vitepress/src/i18n/common/common-zh.ts
@@ -59,7 +59,7 @@ export default {
       CHILDREN: [
         {
           NAME: '文档',
-          PATH: '',
+          PATH: 'docs/3.1.1/docs/BriefTutorial/BriefTutorial.html',
           ID: 'docs',
           IS_OPEN_WINDOW: 1,
         },
