b5150aafd7d9511a29bff5d5cf63b4d3bca24afc
pxq <1456889284@qq.com>
2023-05-10 16:58:45 +0800

    fix:odd2023页面新增议程楼层

---
+++ app/.vitepress/src/views/summit/devday2023/data/agenda1.ts
@@ -0,0 +1,53 @@
+export default {
+  title: '',
+  content: [
+    {
+      lable: '工作会议（13:00-17:10）',
+      id: 'id1',
+      content: [
+        {
+          id: 'id1_1',
+          name: '',
+          content: [
+            {
+              id: 'id1_1_1',
+              time: '13:00-15:30',
+              desc: '技术委员会工作会议',
+              person: [
+                {
+                  name: '',
+                  post: '',
+                },
+              ],
+              detail: '',
+            },
+            {
+              id: 'id1_1_2',
+              time: '14:00-15:30',
+              desc: '用户委员会工作会议',
+              person: [
+                {
+                  name: '',
+                  post: '',
+                },
+              ],
+              detail: '',
+            },
+            {
+              id: 'id1_1_3',
+              time: '15:40-17:10',
+              desc: '理事会工作会议',
+              person: [
+                {
+                  name: '',
+                  post: '',
+                },
+              ],
+              detail: '',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
