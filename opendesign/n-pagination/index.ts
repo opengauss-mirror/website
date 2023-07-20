493f5bb0983d863e25985e15879107607731965b
haml-707 <845831435@qq.com>
2023-07-20 15:17:53 +0800

    fix：删除分页器代码
    This reverts commit df7d2435a58a040ab424dc9e31821b514eea7c6f, reversing
    changes made to 4ecbd9e30ede578930ea6fd4cb1aa2bb7fb60722.

--- opendesign/n-pagination/index.ts
+++
@@ -1,8 +0,0 @@
-// import type { App } from 'vue';
-// import NPagination from './src/pagination';
-
-// NPagination.install = function (app: App) {
-//   app.component(NPagination.name, NPagination);
-// };
-
-// export { NPagination };
