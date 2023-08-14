9159d97c8b99b54a95783083e24c6237a8df34dd
ailoooong <ilong8@qq.com>
2023-08-14 14:22:18 +0800

    fix：软件包下载地址修改

--- app/.vitepress/src/data/download/download2.1.0.ts
+++ app/.vitepress/src/data/download/download2.1.0.ts
@@ -9,13 +9,13 @@ export default [
         name: 'openGauss_2.1.0 企业版',
         table: 'server',
         edition: 'enterprise',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-all.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-all.tar.gz`,
         centos_sha:
           'fe07adef54945d3a09ca71d5fbddfc6c4f0ccc3fe900d692fcb8f57be76d2975',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-all.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-all.tar.gz`,
         aarch_sha:
           '8970f59d5d072cf1d9e23b4e60c7b04b31a4fcc3e8e710d3beb0fe5aa000d4d2',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-all.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-all.tar.gz`,
         x86_sha:
           '9f8eae99e7f545f321bbed06aa666ad71d5561d881f58fa3e1c0479496dc2005',
       },
@@ -39,13 +39,13 @@ export default [
         name: 'openGauss_2.1.0 Enterprise-Edition',
         table: 'server',
         edition: 'enterprise',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-all.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-all.tar.gz`,
         centos_sha:
           'fe07adef54945d3a09ca71d5fbddfc6c4f0ccc3fe900d692fcb8f57be76d2975',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-all.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-all.tar.gz`,
         aarch_sha:
           '8970f59d5d072cf1d9e23b4e60c7b04b31a4fcc3e8e710d3beb0fe5aa000d4d2',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-all.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-all.tar.gz`,
         x86_sha:
           '9f8eae99e7f545f321bbed06aa666ad71d5561d881f58fa3e1c0479496dc2005',
       },
@@ -71,49 +71,49 @@ export default [
     zh: [
       {
         name: 'JDBC_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-JDBC.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-JDBC.tar.gz`,
         centos_sha:
           '1ab94e56845732bc1c0b6984332c7ba7767c9076581a29e446e6505115aa7168',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-JDBC.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-JDBC.tar.gz`,
         aarch_sha:
           '75c8324b05b482016bd37459a94d4b079422676d4ae73373e108297d0292f8c6',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-JDBC.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-JDBC.tar.gz`,
         x86_sha:
           '98bb836253e48a188ca87d521386d30589228c4c7f444969afb0827756e963fb',
       },
       {
         name: 'ODBC_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-ODBC.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-ODBC.tar.gz`,
         centos_sha:
           '401d574601886f4e07512cba2c72b636ee73d96f130cb99111a3d53721c8b994',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-ODBC.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-ODBC.tar.gz`,
         aarch_sha:
           '75c8324b05b482016bd37459a94d4b079422676d4ae73373e108297d0292f8c6',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-ODBC.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-ODBC.tar.gz`,
         x86_sha:
           '8c1fffb29d99da6521c580ef68ba25603a8996ab07c29eb817f16859984c765f',
       },
       {
         name: 'Python-psycopg2_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-x86_64-Python.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-x86_64-Python.tar.gz`,
         centos_sha:
           'bd6c0aaa55e848b9b6dc9facc287805d01ec97201e2dd1d5c89da1851685ead1',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-aarch64-Python.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-aarch64-Python.tar.gz`,
         aarch_sha:
           '422fe29178f0575d9668cf44574da699412ea1204bf3b25b84519be363c2fb7b',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-x86_64-Python.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-x86_64-Python.tar.gz`,
         x86_sha:
           '15fb09f57252dac02bd823279a8cb14442944f6617315a045767de23a528b4b1',
       },
       {
         name: 'libpq_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-Libpq.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-Libpq.tar.gz`,
         centos_sha:
           'f03f58261c03b1b1e286390634aa12b37e9ab67acf3648af40e6c07a2057b6f5',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-Libpq.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-Libpq.tar.gz`,
         aarch_sha:
           '98a8df00d15f4d2b93e9eb88f21483e8f40b1b10acf4cb1b1db1f5ac76627c7e',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-Libpq.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-Libpq.tar.gz`,
         x86_sha:
           '79d72d07f21dbc66a7df4b2d66bd369a94077735ac9176a162f45b267ad3e9e7',
       },
@@ -121,49 +121,49 @@ export default [
     en: [
       {
         name: 'JDBC_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-JDBC.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-JDBC.tar.gz`,
         centos_sha:
           '1ab94e56845732bc1c0b6984332c7ba7767c9076581a29e446e6505115aa7168',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-JDBC.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-JDBC.tar.gz`,
         aarch_sha:
           '75c8324b05b482016bd37459a94d4b079422676d4ae73373e108297d0292f8c6',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-JDBC.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-JDBC.tar.gz`,
         x86_sha:
           '98bb836253e48a188ca87d521386d30589228c4c7f444969afb0827756e963fb',
       },
       {
         name: 'ODBC_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-ODBC.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-ODBC.tar.gz`,
         centos_sha:
           '401d574601886f4e07512cba2c72b636ee73d96f130cb99111a3d53721c8b994',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-ODBC.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-ODBC.tar.gz`,
         aarch_sha:
           '75c8324b05b482016bd37459a94d4b079422676d4ae73373e108297d0292f8c6',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-ODBC.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-ODBC.tar.gz`,
         x86_sha:
           '8c1fffb29d99da6521c580ef68ba25603a8996ab07c29eb817f16859984c765f',
       },
       {
         name: 'Python-psycopg2_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-x86_64-Python.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-x86_64-Python.tar.gz`,
         centos_sha:
           'bd6c0aaa55e848b9b6dc9facc287805d01ec97201e2dd1d5c89da1851685ead1',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-aarch64-Python.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-aarch64-Python.tar.gz`,
         aarch_sha:
           '422fe29178f0575d9668cf44574da699412ea1204bf3b25b84519be363c2fb7b',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-x86_64-Python.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-x86_64-Python.tar.gz`,
         x86_sha:
           '15fb09f57252dac02bd823279a8cb14442944f6617315a045767de23a528b4b1',
       },
       {
         name: 'libpq_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-Libpq.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-Libpq.tar.gz`,
         centos_sha:
           'f03f58261c03b1b1e286390634aa12b37e9ab67acf3648af40e6c07a2057b6f5',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-Libpq.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-Libpq.tar.gz`,
         aarch_sha:
           '98a8df00d15f4d2b93e9eb88f21483e8f40b1b10acf4cb1b1db1f5ac76627c7e',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-Libpq.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-Libpq.tar.gz`,
         x86_sha:
           '79d72d07f21dbc66a7df4b2d66bd369a94077735ac9176a162f45b267ad3e9e7',
       },
@@ -175,13 +175,13 @@ export default [
     zh: [
       {
         name: 'symbol_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-symbol.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-symbol.tar.gz`,
         centos_sha:
           '71ea1ff25e812f96bde7a5aa88b6d6b4cda8cef700e9cb7decc94d553bfb6ae0',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-symbol.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-symbol.tar.gz`,
         aarch_sha:
           'bbf31ad33eee3e959cca4e29a805562812f0f857ad6987800f762e1e99b15b35',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-symbol.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-symbol.tar.gz`,
         x86_sha:
           'a2f8be7e6330e9719b5f8b8e44474ec30139337e61f53c1efec87d8f368ba0c7',
       },
@@ -189,13 +189,13 @@ export default [
     en: [
       {
         name: 'symbol_2.1.0',
-        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-symbol.tar..gz`,
+        centos_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86/openGauss-2.1.0-CentOS-64bit-symbol.tar.gz`,
         centos_sha:
           '71ea1ff25e812f96bde7a5aa88b6d6b4cda8cef700e9cb7decc94d553bfb6ae0',
-        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-symbol.tar..gz`,
+        aarch_url: `${OBS_DOWNLOAD_LINK}2.1.0/arm/openGauss-2.1.0-openEuler-64bit-symbol.tar.gz`,
         aarch_sha:
           'bbf31ad33eee3e959cca4e29a805562812f0f857ad6987800f762e1e99b15b35',
-        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-symbol.tar..gz`,
+        x86_url: `${OBS_DOWNLOAD_LINK}2.1.0/x86_openEuler/openGauss-2.1.0-openEuler-64bit-symbol.tar.gz`,
         x86_sha:
           'a2f8be7e6330e9719b5f8b8e44474ec30139337e61f53c1efec87d8f368ba0c7',
       },
