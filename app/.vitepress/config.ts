70b51b75ee6a4968b383f734c479551f421c2bfe
pxq <1456889284@qq.com>
2023-09-13 17:07:19 +0800

    fix:整体seo优化

--- app/.vitepress/config.ts
+++ app/.vitepress/config.ts
@@ -18,6 +18,13 @@ const config: UserConfig = {
       },
     ],
     [
+      'meta',
+      {
+        name: 'keywords',
+        content: 'openGauss,开源数据库,openGauss社区官网,开源社区',
+      },
+    ],
+    [
       'script',
       {
         src: 'https://hm.baidu.com/hm.js?ace49cc6c2f3d0542e97ce86732094dc',
@@ -43,13 +50,13 @@ const config: UserConfig = {
       lang: 'zh',
       title: 'openGauss',
       description:
-        'openGauss is an enterprise-grade open source relational database with high-performance, high-security, high-reliability. openGauss is also an open source database platform that encourages community contribution and collaboration.',
+        'openGauss是一个高性能、高安全、高可用、高智能的企业级开源关系数据库。openGauss也是一个鼓励社区贡献和协作的开源数据库平台。',
     },
     '/zh/': {
       lang: 'zh',
       title: 'openGauss',
       description:
-        'openGauss is an enterprise-grade open source relational database with high-performance, high-security, high-reliability. openGauss is also an open source database platform that encourages community contribution and collaboration.',
+        'openGauss是一个高性能、高安全、高可用、高智能的企业级开源关系数据库。openGauss也是一个鼓励社区贡献和协作的开源数据库平台。',
     },
     '/en/': {
       lang: 'en',
