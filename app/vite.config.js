96e1018b64bdb273b38551de4a27b75f65d35d0a
pxq <1456889284@qq.com>
2023-10-27 09:11:49 +0800

    fix: 搜索接口地址更新

--- app/vite.config.js
+++ app/vite.config.js
@@ -65,7 +65,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api-meeting/, ''),
       },
       '/api-search/': {
-        target: 'https://doc-search.opengauss.org/',
+        target: 'https://doc-search-opengauss.osinfra.cn/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api-search/, ''),
       },
