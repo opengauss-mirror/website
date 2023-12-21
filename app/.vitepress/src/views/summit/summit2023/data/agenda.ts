a822afab5d05414ad6e3724a71e451983cd2ca30
pxq <1456889284@qq.com>
2023-12-21 16:47:42 +0800

    feat: 峰会新增分论坛议程数据

--- app/.vitepress/src/views/summit/summit2023/data/agenda.ts
+++ app/.vitepress/src/views/summit/summit2023/data/agenda.ts
@@ -330,12 +330,563 @@ export default [
           content: [
             {
               id: 'agenda28_2_1',
-              name: '',
+              name: '海量数据',
               content: [
                 {
                   id: 'agenda28_2_1_1',
-                  time: '14:00-16:00',
-                  desc: '敬请期待',
+                  time: '14:00-14:05',
+                  desc: '主持人开场',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '白玥',
+                      post: '海量数据市场总监',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:05-14:15',
+                  desc: '领导致辞',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '熊伟',
+                      post: '开放原子开源基金会TOC副主席',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:15-14:30',
+                  desc: '构建开源数据库生态，保障软件供应链安全',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '翟艳芬',
+                      post: '中国软件评测中心信发中心主任、基础软件质量控制与技术评价工信部重点实验室常务副主任',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:30-14:45',
+                  desc: 'Vastbase融合创新之基于“资源池化架构”的应用与实践分享',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '孙久江',
+                      post: '海量数据技术专家',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:45-15:05',
+                  desc: '聚力前行，创维携手海量数据澎湃制造产业数据库新动能',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '吴伟',
+                      post: '创维集团执行董事、总工程师',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:05-15:25',
+                  desc: '化繁为简，国企云联合Vastbase助力企业数字化应用创新',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '于水情',
+                      post: '北控数科售前咨询总监',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:25-15:40',
+                  desc: 'Vastcube G3000数据库一体机助推智能化、数字化加速向前',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '吉祥',
+                      post: '海量数据解决方案专家',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:40-16:00',
+                  desc: '海量数据库2023年度优秀合作伙伴颁奖',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '',
+                      post: '',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '16:00-16:15',
+                  desc: '聚势而强，共建自主创新产业新生态',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '刁新',
+                      post: '拓维信息智能计算事业群 副总裁',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '16:15-16:30',
+                  desc: '超图携手海量数据打造SuperMap iServer 10i联合解决方案',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '陈勇',
+                      post: '超图研究院副院长',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '16:30-17:00',
+                  desc: '圆桌论坛：数据库生态建设与应用机遇',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '',
+                      post: '',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '17:00-17:05',
+                  desc: '幸运抽奖',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '',
+                      post: '',
+                    },
+                  ],
+                  detail: '',
+                },
+              ],
+            },
+            {
+              id: 'agenda28_2_1',
+              name: '云和恩墨',
+              content: [
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:00-14:05',
+                  desc: '社区领导致辞',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '胡正策',
+                      post: 'openGauss社区理事长',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:05-14:15',
+                  desc: '领导致辞',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '盖国强',
+                      post: '云和恩墨创始人兼总经理、鲲鹏MVP',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:15-14:35',
+                  desc: '笃行致远，MogDB数据库技术与架构创新',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '张皖川',
+                      post: '云和恩墨·本原数据技术合伙人、数据库产品事业部总经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:35-14:50',
+                  desc: '保险行业国产数据库应用实践',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '张荣',
+                      post: '中科软技术总监',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:50-15:05',
+                  desc: '泛微在金融领域的实践与探索',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '杨昭',
+                      post: '泛微战略客户部总经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:05-15:25',
+                  desc: 'MogDB在用户核心系统自主创新中的应用之法与成功之道',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '罗海雄',
+                      post: '云和恩墨客户成功部研发架构师',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:25-15:40',
+                  desc: 'openGauss的top类监控新工具ogtop详解',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '徐杨',
+                      post: '民生银行数据库技术专家',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:40-15:45',
+                  desc: '云和恩墨zData X & 百信鲲鹏服务器联合解决方案签约发布仪式',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '孙培源',
+                      post: '云和恩墨战略渠道部总经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:45-15:55',
+                  desc: '百信&云和恩墨全栈国产数据库一体机解决方案',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '李京',
+                      post: '百信信息技术有限公司解决方案总经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:55-16:15',
+                  desc: 'MogDB Cube数据库一体机架构解析与创新实践',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '张程伟',
+                      post: '云和恩墨·本原数据技术合伙人',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '16:15-16:20',
+                  desc: '幸运抽奖&合影留念',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '',
+                      post: '',
+                    },
+                  ],
+                  detail: '',
+                },
+              ],
+            },
+            {
+              id: 'agenda28_2_1',
+              name: '南大通用',
+              content: [
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:00-14:05',
+                  desc: '社区领导致辞',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '蔡亚杰',
+                      post: 'openGauss社区秘书长',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:10-14:20',
+                  desc: '领导致辞',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '张益',
+                      post: '南大通用GBase 8c产品总经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:20-14:35',
+                  desc: '华为ICT学院—数据库人才发展战略',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '孙刚',
+                      post: '华为ICT人才伙伴发展部部长',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:35-15:00',
+                  desc: '信息技术应用与创新工作一体化能力平台',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '沙勇',
+                      post: '时代亿信CTO',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:00-15:25',
+                  desc: '一专多能 GBase 8c向量引擎揭秘',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '王涛',
+                      post: '南大通用GBase 8c技术架构师',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:25-15:50',
+                  desc: '携手GBase构建智慧业务基础架构',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '詹年科',
+                      post: '宝兰德技术总监',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:50-16:15',
+                  desc: 'openGauss资源池化特性介绍',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '贺承汉',
+                      post: 'openGauss开源数据库总工程师',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '16:15-16:40',
+                  desc: '易华录一网统管解决方案以数据赋能城市运行高效管理',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '唐月',
+                      post: '易华录智慧城市事业部产品总监',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '16:40-16:45',
+                  desc: '幸运抽奖&合影留念',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '',
+                      post: '',
+                    },
+                  ],
+                  detail: '',
+                },
+              ],
+            },
+            {
+              id: 'agenda28_2_1',
+              name: '神舟通用&中国移动',
+              content: [
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:00-14:05',
+                  desc: '主持人开场',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '王天访',
+                      post: '神舟通用数据技术有限公司售前总监',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:05-14:10',
+                  desc: '社区领导致辞',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '江大勇',
+                      post: '开放原子开源基金会理事长助理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:10-14:15',
+                  desc: '神舟通用公司领导致辞',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '顾云苏',
+                      post: '神舟通用数据技术有限公司总经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:15-14:20',
+                  desc: '中国移动领导致辞',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '陈国',
+                      post: '中国移动信息技术中心副总经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:20-14:50',
+                  desc: '神通数据库openGauss版产品研制及特点',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '刘勇生',
+                      post: '神舟通用数据技术有限公司重庆研发中心总经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '14:50-15:00',
+                  desc: '神通数据库openGauss版产品应用案例分享',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '张御博',
+                      post: '神舟通用数据技术有限公司售前经理',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:00-15:30',
+                  desc: '中国移动磐维数据库产品技术分享',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '崔凯峰',
+                      post: '中国移动信息技术中心平台能力中心组件研发组总监&磐维数据库架构师',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:30-15:50',
+                  desc: '浙江移动磐维数据库应用案例分享',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '郭岳',
+                      post: '中国移动通信集团浙江有限公司信数部智慧中台运营中心副主任',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '15:50-16:10',
+                  desc: '中国移动磐维数据库助力客户数字化转型',
+                  person: [
+                    {
+                      id: 'agenda28_2_1_1_1',
+                      name: '王建业',
+                      post: '中国移动信息技术中心平台能力中心解决方案专家',
+                    },
+                  ],
+                  detail: '',
+                },
+                {
+                  id: 'agenda28_2_1_1',
+                  time: '16:10-16:20',
+                  desc: '幸运抽奖&合影留念',
                   person: [
                     {
                       id: 'agenda28_2_1_1_1',
