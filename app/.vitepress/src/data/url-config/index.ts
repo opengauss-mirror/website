793aa177da50496bea64ef65829b93807a224341
pxq <1456889284@qq.com>
2024-01-26 10:11:52 +0800

    feat: summit2023议程新增演讲材料下载

--- app/.vitepress/src/data/url-config/index.ts
+++ app/.vitepress/src/data/url-config/index.ts
@@ -1,4 +1,5 @@
 // 涉及的公网路径及邮箱地址的配置文件
+const OBS_Summit_PPT='https://obs-for-opengauss-summit.obs.cn-north-4.myhuaweicloud.com/'
 const GAUSS_YEAR ='https://summary.opengauss.org/'
 const GAUSS_EMAIL = 'contact@opengauss.org'; // gauss邮箱
 const GAUSS_CERTIFICATE_EMAIL = 'common@certificate.opengauss.org'; // gauss证书相关联系邮箱
@@ -63,6 +64,7 @@ const SHIMO_LINK = 'https://shimo.im'; // 石墨报名
 const SUMMIT2023_JOIN = 'https://e-campaign.huawei.com/t/BbuQr2'; // SUMMIT报名
 
 export {
+  OBS_Summit_PPT,
   GAUSS_YEAR,
   GAUSS_EMAIL,
   GAUSS_CERTIFICATE_EMAIL,
