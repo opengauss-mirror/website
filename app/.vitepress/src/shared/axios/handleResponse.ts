a3fbadac16469475580ea21c9d8da4b27b6b9f64
邦邦邦邦 <15622356989@163.com>
2024-02-05 17:15:56 +0800

    fix: axios封装类型优化

--- app/.vitepress/src/shared/axios/handleResponse.ts
+++ app/.vitepress/src/shared/axios/handleResponse.ts
@@ -7,7 +7,7 @@ export default (response: AxiosResponse) => {
     return response;
   } else {
     const code = parseInt(response.data && response.data.code);
-    const message = response.data?.msg || 'Error';
+    const message = (response.data || {}).msg;
 
     return {
       code,
--- app/.vitepress/src/shared/axios/index.ts
+++ app/.vitepress/src/shared/axios/index.ts
@@ -4,6 +4,7 @@ import type {
   AxiosError,
   AxiosInstance,
   AxiosRequestConfig,
+  AxiosRequestHeaders,
   AxiosResponse,
   AxiosStatic,
 } from 'axios';
@@ -16,14 +17,10 @@ import { tokenFailIndicateLogin } from '../login';
 
 interface RequestConfig<D = any> extends AxiosRequestConfig {
   data?: D;
-  $doException?: boolean; // 是否弹出错误提示框
+  showLoading?: boolean; // 加载时是否出现Loading框，默认为false
+  showError?: boolean; // 请求报错是否出现错误提示，默认为false
   global?: boolean; // 是否为全局请求， 全局请求在清除请求池时，不清除
-  noLoading?: boolean; // 是否使用loading
 }
-
-// 全局loading
-let loadingInstance: LoadingInstance | null = null;
-let loadingCount = 0;
 
 interface RequestInstance extends AxiosInstance {
   removeRequestInterceptor(): void;
@@ -66,6 +63,14 @@ interface RequestInstance extends AxiosInstance {
   ): Promise<R>;
 }
 
+interface InternalRequestConfig extends RequestConfig {
+  headers: AxiosRequestHeaders;
+}
+
+// 全局loading
+let loadingInstance: LoadingInstance | null = null;
+let loadingCount = 0;
+
 /**
  * request是基于axios创建的实例，实例只有常见的数据请求方法，没有axios.isCancel/ axios.CancelToken等方法，
  * 也就是没有**取消请求**和**批量请求**的方法。
@@ -81,8 +86,8 @@ const pendingPool: Map<string, any> = new Map();
  * 请求拦截
  */
 const requestInterceptorId = request.interceptors.request.use(
-  (config: AxiosRequestConfig) => {
-    if (loadingCount === 0 && !(config as RequestConfig).noLoading) {
+  (config: InternalRequestConfig) => {
+    if (config.showLoading) {
       loadingInstance = ElLoading.service({
         fullscreen: true,
         target: 'body',
@@ -90,18 +95,23 @@ const requestInterceptorId = request.interceptors.request.use(
         background: 'transparent',
       });
     }
-    loadingCount++;
+
+    // 存储请求信息
     // 定义取消请求
     config.cancelToken = new axios.CancelToken((cancelFn) => {
       if (!config.url) {
         return;
       }
-      // 存储到请求池
-      pendingPool.set(config.url, {
-        cancelFn,
-        global: (config as RequestConfig).global,
-      });
-      // }
+      // 如果已请求，则取消重复请求
+      if (pendingPool.has(config.url)) {
+        cancelFn(`${config.url}请求重复`);
+      } else {
+        // 存储到请求池
+        pendingPool.set(config.url, {
+          cancelFn,
+          global: config.global,
+        });
+      }
     });
     return config;
   },
@@ -115,12 +125,12 @@ const requestInterceptorId = request.interceptors.request.use(
  */
 const responseInterceptorId = request.interceptors.response.use(
   (response: AxiosResponse) => {
-    loadingCount--;
-    if (loadingCount === 0 && loadingInstance) {
+    const config = response.config as InternalRequestConfig;
+
+    if (loadingInstance) {
       loadingInstance.close();
       loadingInstance = null;
     }
-    const { config } = response;
     // 请求完成，移除请求池
     if (config.url) {
       pendingPool.delete(config.url);
@@ -131,39 +141,46 @@ const responseInterceptorId = request.interceptors.response.use(
   (err: AxiosError) => {
     if (loadingInstance) {
       loadingInstance.close();
+      loadingInstance = null;
     }
-    const { config } = err;
-    if (!(config as RequestConfig).$doException) {
-      ElMessage({
-        type: 'error',
-        message: err.toString(),
-      });
-    }
+
+    const config = err.config as InternalRequestConfig;
+
     // 非取消请求发生异常，同样将请求移除请求池
     if (!axios.isCancel(err) && config.url) {
       pendingPool.delete(config.url);
     }
 
     if (err.response) {
-      err = handleError(err);
-      // 无效token跳转登录
-      if (err.code === '401') {
-        tokenFailIndicateLogin();
+      if (err.stack && err.stack.includes('timeout')) {
+        err.message = '请求超时!';
       }
-    }
-    // 没有response(没有状态码)的情况
-    // 如: 超时；断网；请求重复被取消；主动取消请求；
-    else {
-      // 被取消的请求
-      if (axios.isCancel(err)) {
-        throw new axios.Cancel(err.message || `请求'${config.url}'被取消`);
-      } else if (err.stack && err.stack.includes('timeout')) {
+      err = handleError(err);
+    } else {
+      if (err.stack && err.stack.includes('timeout')) {
         err.message = '请求超时!';
       } else {
         err.message = '连接服务器失败!';
       }
+
+      if (axios.isCancel(err)) {
+        throw new axios.Cancel(err.message || `请求'${config.url}'被取消`);
+      }
     }
-    return Promise.reject(err);
+
+    if (config.showError) {
+      ElMessage({
+        type: 'error',
+        message: err.message.toString(),
+      });
+    }
+
+    // token过期，重新登录
+    if (err.response?.status === 401) {
+      tokenFailIndicateLogin();
+    }
+
+    return Promise.reject(err).catch(() => {});
   }
 );
 // 移除全局的请求拦截器
@@ -209,10 +226,5 @@ request.removeRequestInterceptor = removeRequestInterceptor;
 request.removeResponseInterceptor = removeResponseInterceptor;
 request.clearPendingPool = clearPendingPool;
 
-export {
-  intactRequest,
-  request,
-  AxiosResponse,
-  RequestConfig,
-  RequestInstance,
-};
+export { intactRequest, request };
+export type { AxiosResponse, RequestConfig, RequestInstance };
