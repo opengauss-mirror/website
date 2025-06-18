ea1027d2524cd6ee8592474f4cc624adc81641db
ailoooong <ilong8@qq.com>
2025-06-18 18:12:47 +0800

    feat: 更新友情链接

--- app/.vitepress/src/data/home/link.ts
+++ app/.vitepress/src/data/home/link.ts
@@ -1,28 +1,24 @@
 import openeuler from '@/assets/category/home/link/light/openeuler.png';
-import openlookeng from '@/assets/category/home/link/light/openlookeng.png';
+import openfuyao from '@/assets/category/home/link/light/openfuyao.png';
 import kunpeng from '@/assets/category/home/link/light/kunpeng.png';
 import songshuhui from '@/assets/category/home/link/light/songshuhui.png';
 import mindspore from '@/assets/category/home/link/light/mindspore.png';
 
 import openeuler_dark from '@/assets/category/home/link/dark/openeuler.png';
-import openlookeng_dark from '@/assets/category/home/link/dark/openlookeng.png';
+import openfuyao_dark from '@/assets/category/home/link/dark/openfuyao.png';
 import kunpeng_dark from '@/assets/category/home/link/dark/kunpeng.png';
 import songshuhui_dark from '@/assets/category/home/link/dark/songshuhui.png';
 import mindspore_dark from '@/assets/category/home/link/dark/mindspore.png';
+import openubmc from '@/assets/category/home/link/light/openubmc.png';
+import openubmc_dark from '@/assets/category/home/link/dark/openubmc.png';
 
-import {
-  EULER_LINK,
-  MINDSPORE_LINK,
-  LOOKENG_LINK,
-  KUNPENG_LINK,
-  BBSCSDN_LINK,
-} from '@/data/url-config';
+import { EULER_LINK, MINDSPORE_LINK, LOOKENG_LINK, KUNPENG_LINK, BBSCSDN_LINK, EULER_EN_LINK, OPENUBMC_URL, OPENFUYAO_URL } from '@/data/url-config';
 export default [
   {
     img: openeuler,
     imgDark: openeuler_dark,
     path: EULER_LINK,
-    pathEn: `${EULER_LINK}en/`,
+    pathEn: `${EULER_EN_LINK}/en/`,
   },
   {
     img: mindspore,
@@ -31,10 +27,16 @@ export default [
     pathEn: `${MINDSPORE_LINK}en/`,
   },
   {
-    img: openlookeng,
-    imgDark: openlookeng_dark,
-    path: `${LOOKENG_LINK}zh/`,
-    pathEn: `${LOOKENG_LINK}en/`,
+    img: openubmc,
+    imgDark: openubmc_dark,
+    path: OPENUBMC_URL,
+    pathEn: OPENUBMC_URL,
+  },
+  {
+    img: openfuyao,
+    imgDark: openfuyao_dark,
+    path: OPENFUYAO_URL,
+    pathEn: LOOKENG_LINK,
   },
   {
     img: kunpeng,
