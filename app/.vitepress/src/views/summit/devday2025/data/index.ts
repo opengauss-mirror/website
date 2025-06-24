7abb12d761bf93636704372b106eb100d0be8b84
wuming230 <1819845645@qq.com>
2025-06-24 17:17:52 +0800

    fix: ODD2025议程修改

--- app/.vitepress/src/views/summit/devday2025/data/index.ts
+++ app/.vitepress/src/views/summit/devday2025/data/index.ts
@@ -17,7 +17,7 @@ export default {
     titleBg: 'INTRODUCTION',
     desc: [
       'openGauss Developer Day 2025是openGauss社区发起的开发者大会，旨在持续推动数据库领域的创新和突破。本次大会将全面展示openGauss 7.0.0 创新版的技术特性、openGauss 在内核、AI和四高（高性能、高可用、高智能、高安全）能力上的最新技术成果。',
-      '本次大会也是 openGauss 社区的年度大型工作会议，值此开源五周年之际，openGauss社区邀请开发者、用户齐聚一堂，共同讨论下一个版本的技术路线与为了演进方向，携手共建数智化的数据库新生态。',
+      '本次大会也是 openGauss 社区的年度大型工作会议，值此开源五周年之际，openGauss社区邀请开发者、用户齐聚一堂，共同讨论下一个版本的技术路线与演进方向，携手共建数智化的数据库新生态。',
     ],
   },
   agenda: {
