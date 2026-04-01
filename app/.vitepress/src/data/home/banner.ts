73af616c1c209f224f73387a816fce18bf366272
zjwmiao <1723168479@qq.com>
2026-04-01 17:01:56 +0800

    fix: 2025年度报告banner文字颜色调整

--- app/.vitepress/src/data/home/banner.ts
+++ app/.vitepress/src/data/home/banner.ts
@@ -98,6 +98,7 @@ export default {
       link: '/zh/news/2026-01-16/',
       target: '_blank',
       title: 'openGauss 开源社区2025年度报告',
+      isLightBg: true,
       titleMb: [''],
       subtitle: '',
       desc: [''],
