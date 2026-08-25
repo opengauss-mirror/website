eb7ebbe35e59a08a0a3a232bac9cd12b54eaf506
zjwmiao <1723168479@qq.com>
2026-08-25 19:56:48 +0800

    feat: 首页社区伙伴用模板组件替换

--- app/.vitepress/src-new/views/home/HomePartners.vue
+++ app/.vitepress/src-new/views/home/HomePartners.vue
@@ -1,33 +1,57 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import { storeToRefs } from 'pinia';
+import { OLogoSwiper, OLogoSwiperItems, OPlusConfigProvider } from '@opendesign-plus/components';
+import type { OLogoSwiperItemT } from '@opendesign-plus/components';
+import homeContent from '#content/home';
 import AppSection from '~@/components/AppSection.vue';
-import HomeSwiper from './HomeSwiper.vue';
-import homeConfig from '@/data/home/';
 import { useLocale } from '~@/composables/useLocale';
+import { useCommon } from '~@/stores/common';
 
 const { t } = useLocale();
+const { theme } = storeToRefs(useCommon());
 
-const data = homeConfig.organization;
+type OrganizationItemT = (typeof homeContent)['organization'][number];
 
-const partLen = Math.floor(data.length / 3);
+const mapFunc = (item: OrganizationItemT): OLogoSwiperItemT => ({
+  logo: item.img_light,
+  logoDark: item.img_dark,
+});
 
-const displayData = data
-  .reduce(
-    (acc, curr, index) => {
-      const groupIndex = Math.min(Math.floor(index / partLen), 2);
-      acc[groupIndex].push(curr);
-      return acc;
-    },
-    [[], [], []] as (typeof data)[number][][]
-  )
-  .map((part) => [...part, ...part]);
+const ROW_COUNT = 3;
+
+const rows = computed<OLogoSwiperItemT[][]>(() => {
+  const list = homeContent.organization.map(mapFunc);
+  const partLen = Math.floor(list.length / ROW_COUNT) || 1;
+  const buckets: OLogoSwiperItemT[][] = Array.from({ length: ROW_COUNT }, () => []);
+  list.forEach((item, index) => {
+    const groupIndex = Math.min(Math.floor(index / partLen), ROW_COUNT - 1);
+    buckets[groupIndex].push(item);
+  });
+  return buckets;
+});
+
+const durationMap = computed(() => rows.value.map(row => Math.floor(row.length / 4) * 30));
+
+const swiperTheme = computed<'light' | 'dark'>(() => (theme.value === 'dark' ? 'dark' : 'light'));
 </script>
 
 <template>
   <AppSection class="home-partner" :title="t('home.partners.title')" :full="true">
-    <HomeSwiper v-for="(part, index) in displayData" :key="index" :reverse-direction="index === 1" :data="part" class="partner-swiper"></HomeSwiper>
-    <template #footer>
-      <p class="partner-tips">{{ t('home.partners.orderDesc') }}</p>
-    </template>
+    <OPlusConfigProvider :theme="swiperTheme">
+      <OLogoSwiper class="partner-swiper">
+        <OLogoSwiperItems
+          v-for="(row, index) in rows"
+          :key="index"
+          :data="row"
+          :reverse="index % 2 === 1"
+          :duration="durationMap[index]"
+        />
+        <template #tip>
+          <p class="partner-tip">{{ t('home.partners.orderDesc') }}</p>
+        </template>
+      </OLogoSwiper>
+    </OPlusConfigProvider>
   </AppSection>
 </template>
 
@@ -35,32 +59,10 @@ const displayData = data
 .home-partner {
   margin: 0 auto;
 }
-.partner-swiper:not(:first-child) {
-  margin-top: 24px;
-  @include respond-to('laptop') {
-    margin-top: 20px;
-  }
-  @include respond-to('pad_h') {
-    margin-top: 16px;
-  }
-  @include respond-to('<=pad_v') {
-    margin-top: 12px;
-  }
-}
 
-.parterner-tips {
+.partner-tip {
   @include tip1;
   text-align: center;
   color: var(--o-color-info3);
-}
-</style>
-
-<style lang="scss">
-@include in-dark {
-  .partner-swiper {
-    .o-figure img {
-      filter: none;
-    }
-  }
 }
 </style>
